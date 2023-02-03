d45d67d51c46a9f944b7e4f7f6d319bf8da5e7e2
Linh Dinh <nhlinh1803@gmail.com>
2023-02-02 20:45:37 -0800

    Completed Java Lab 003

--- src/GettingHotInHere.java
+++ src/GettingHotInHere.java
@@ -1,3 +1,12 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Linh Dinh
+ *
+ * @since Version 1.0
+ *
+ */
+
 import java.util.Scanner;
 
 public class GettingHotInHere {
@@ -7,8 +16,17 @@ public class GettingHotInHere {
      **/
     public static void main(String[] args) {
         // The instantiation code for a Scanner instance.
-        Scanner scanner = new Scanner(System.in);
+        final double FREEZING_POINT = 32.0; //declare final variable
+        final double CONSTANT = 5.0/9.0; //declare final variable
+        Scanner scanner = new Scanner(System.in); //read input from user
         System.out.print("What is the temperature in °F: ");
-        int number = scanner.nextInt();
+        int number = scanner.nextInt(); //read an integer from user
+        double celsius = (number - FREEZING_POINT)*(CONSTANT);
+        int celsiusInt = (int)celsius; // convert the value of Celsius to integer
+        System.out.println(number + "°F");
+        System.out.printf("%f°C%n",celsius);
+        System.out.println(celsiusInt + "°C");
+        System.out.printf("If it were 2°C warmer it would be: %f°C",(2.0+celsius));
+
     }
 }
--- src/MrRoboto.java
+++ src/MrRoboto.java
@@ -1,3 +1,11 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Linh Dinh
+ *
+ * @since Version 1.0
+ *
+ */
 // Import the Scanner tool used for reading (All external packages/libraries/apis are imported similarly this)
 import java.util.Scanner;
 
@@ -15,5 +23,9 @@ public class MrRoboto {
 
         System.out.print("My name is Mr. Roboto, are you Kilroy? ");
         // Write your program here
+        /* declare a variable named response
+        and make it get a line of input read by the Scanner variable.*/
+        String response = scanner.nextLine();
+        System.out.println(domo + response); //concatenate input to String variable domo
     }
 }
